69b20678bd188525374241e849270d8bba8ff949
FreedomChase <freedom.chase@hotmail.com>
2021-03-26 19:32:03 +0100

    minor js update.
    generate products from the database using jquery post

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,38 @@
 function openProduct(productBox, elmnt, color) {
-  // Hide all elements with class="tabcontent" by default */
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
 
-  // Remove the background color of all tablinks/buttons
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  }
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
 
-  // Show the specific tab content
-  document.getElementById(productBox).style.display = "block";
+    // Show the specific tab content
+    document.getElementById(productBox).style.display = "block";
 
-  // Add the specific color to the button used to open the tab content
-  elmnt.style.backgroundColor = color;
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = color;
 }
 
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+(function () {
+    $.post("clients/share/featured_products", {send: ''}, function (result) {
+        $("#products_container").html(result);
+        document.getElementById("defaultOpen").click();
+    });
+})();
 
 
 function myFunction() {
-  var x = document.getElementById("ingredient");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+    var x = document.getElementById("ingredient");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
